a242e1aab49089f3d2e6d0e98d98c178db561ef0
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-10-23 14:02:18 +0330

    update underDevJS by @AliMD

--- htdocs/scripts/underdev.js
+++ htdocs/scripts/underdev.js
@@ -1,16 +1,21 @@
 // Zepto/jQuery fadeLoop plugin for fade slide show effects by ali.md
 
 (function($){
-	var ease = !!window.Zepto ? 'ease-out' : 'swing';
+	var	ease = !!window.Zepto ? 'ease-out' : 'swing';
 	$.extend($.fn,{
 		fadeLoop :function(options){
 
 			options=$.extend({
 				duration : 2500,
-				delay : 2000,
-				startIndex : -1,
-				fadeFirstImage : true
+				freez : 1500,
+				delay : 10,
+				startIndex : 0,
+				fadeFirstImage : true,
+				zIndex : -3,
+				zIndexAct : -2
 			},options);
+
+			options.startIndex--;
 
 			var nextPic,
 				pics    =this,
@@ -19,29 +24,44 @@
 				fadeIn  ={opacity:1},
 				fadeOut ={opacity:0};
 
-			pics.css(fadeOut);
-			options.fadeFirstImage || pics.eq(0).css(fadeIn);
+			var nextPic = function(){
+				pics.eq(indx).animate(fadeOut,options.duration,ease,function(){
+					 $(this).css({'z-index':options.zIndex});
 
-			(nextPic=function(){
-				pics.eq(indx).animate(fadeOut,options.duration,ease);
-				indx=indx<plen-1?indx+1:0;
-				pics.eq(indx).animate(fadeIn,options.duration,ease,function(){
-					setTimeout(nextPic,options.delay);
 				});
-			})();
+				indx=indx<plen-1?indx+1:0;
+				setTimeout(function(){
+					pics.eq(indx).css({'z-index':options.zIndexAct}).animate(fadeIn,options.duration,ease,function(){
+						setTimeout(nextPic,options.freez);
+					});
+				},options.delay+10);
+			};
+
+			pics.css(fadeOut).css({'z-index':options.zIndex});
+
+			if(!options.fadeFirstImage){
+				pics.eq(0).css(fadeIn).css({'z-index':options.zIndexAct});
+				indx++;
+				setTimeout(nextPic,options.freez);
+			}else{
+				nextPic();
+			}
 		}
 	});
 
 	$(function(){
 		$('.backimg > div').fadeLoop({
-			duration : 1500,
-			delay : 2000
+			delay : 200,
+			freez : 3000,
+			duration : 1300,
+			zIndex : -3,
+			zIndexAct : -2,
+			startIndex : 0,
+			fadeFirstImage : true
 		});
+
 		$('section.slideshow p.desc').animate({'left':'0px','opacity':'1'},1200,ease);
 		$('section.slideshow h1.title').animate({'left':'0px','opacity':'1'},900,ease);
 	});
-
-	
-	
 
 })(window.Zepto || window.jQuery);
