ca0b33e0ff383c828ba3534bb456f837afdcd6d8
Ali Mihandoost <i@ali.md>
2012-10-25 12:54:30 +0330

    add analytic + lt-ie8

--- htdocs/inc/description.php
+++ htdocs/inc/description.php
@@ -1,3 +1,2 @@
-	<?php dynamic_content('description_title',"<h1 class='title'>",'</h1>') ?>
-	
-	<?php dynamic_content('description_text',"<p class='desc'>",'</p>') ?>
+<?php dynamic_content('description_title',"<h1 class='title'>",'</h1>') ?>
+<?php dynamic_content('description_text',"<p class='desc'>",'</p>') ?>
