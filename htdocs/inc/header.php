7114fdefc27854ac993cdc0fb4915ecffe490256
Ali Mihandoost <i@ali.md>
2012-10-25 12:07:16 +0330

    modernizer

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -1,12 +1,19 @@
 <?php require_once('dynamic_content.php'); ?>
-<!doctype html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>		<html lang="en-US" dir="ltr" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>			<html lang="en-US" dir="ltr" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>			<html lang="en-US" dir="ltr" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->	<html lang="en-US" dir="ltr" class="no-js"> <!--<![endif]-->
 <html lang="en-US">
 <head>
 	<meta charset="utf-8" />
-	<?php dynamic_content('page_title',"<title>",'</title>') ?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width" /
+	<?php dynamic_content('page_title',"<title>",'</title>'); ?>
+	<?php dynamic_content('description_text','<meta name="description" content="','" />'); ?>
 	<link rel="stylesheet" type="text/css" href="1styles.css" />
 	<link rel="stylesheet" type="text/css" href="style.css" />
-	<script type="text/javascript" src="scripts/html5shiv.js"></script>
+	<script src="scripts/modernizr.js"></script>
 	<script type="text/javascript"> document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>'); </script>
 	<script type="text/javascript" src="scripts/underdev.js"></script>
 </head>
