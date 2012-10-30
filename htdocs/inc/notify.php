6235af18758498c1f6eff54b37261a1cbcd86327
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-10-30 13:17:26 +0330

    add notifyMe

---
+++ htdocs/inc/notify.php
@@ -0,0 +1,5 @@
+	<form id="notify-form" action="notify.php" method="post" target='ifrm'>
+		<input class="notify" type="text" name="name" placeholder="Name" />
+		<input class="btn" type="submit" name="submit" value="Send" />
+	</form>
+	<iframe id='notify-ifrm' name='notify-ifrm' src="" frameborder="0" scrolling="no"></iframe>
