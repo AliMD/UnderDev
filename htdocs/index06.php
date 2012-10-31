5518f521c02dfb33161dacf1ae410a0b5614dff1
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-10-31 10:58:48 +0330

    add ProgressBar in inc

--- htdocs/index06.php
+++ htdocs/index06.php
@@ -1,7 +1,8 @@
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/slideshow.php'; ?>
-<div class="container">
-	<?php include 'inc/description.php'; ?>
-	<?php include 'inc/logo.php'; ?>
+
+<div class="popup">
+	<?php include 'inc/progressbar.php'; ?>
 </div>
+
 <?php include 'inc/footer.php'; ?>
