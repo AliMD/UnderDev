f39cf4589cf18dbeb92e0fdd356740f3261af74b
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-10-31 10:52:42 +0330

    add index06

---
+++ htdocs/index06.php
@@ -0,0 +1,7 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/slideshow.php'; ?>
+<div class="container">
+	<?php include 'inc/description.php'; ?>
+	<?php include 'inc/logo.php'; ?>
+</div>
+<?php include 'inc/footer.php'; ?>
