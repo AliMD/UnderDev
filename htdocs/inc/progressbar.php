caa6a5ab28b2b5a1ccc066b548137ec288a371cd
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-10-31 16:16:30 +0330

    add transition and deley

--- htdocs/inc/progressbar.php
+++ htdocs/inc/progressbar.php
@@ -1,6 +1,6 @@
 <div class="progressbar">
 	<div class="percentHolder">
-		<div class="percent"><span>40%</span></div>
+		<div class="percent"><span>65%</span></div>
 	</div>
 	<div class="bar"></div>
 </div>
